0e2931f6b541b88d11b1390a7524c5b77d2fff1a
Boniface Yogendran <yogi@Bonifaces-MacBook-Pro.local>
2025-06-02 07:44:33 -0400

    readme and validation script

---
+++ validation_script.py
@@ -0,0 +1,49 @@
+import starpu
+import asyncio
+from invoke import run
+import cudaq
+
+
+def func():
+    return 12
+
+
+async def main():
+    # submit function "hello"
+    fut = starpu.task_submit()(func)
+    res = await fut
+
+    assert res == 12, "The result should be 12"
+
+
+def validate_nvidia_smi():
+    try:
+        res = run("nvidia-smi --query-gpu=gpu_name --format=csv,noheader")
+        GPU_NAME = res.stdout.strip()
+        assert GPU_NAME, "No GPU detected by nvidia-smi"
+    except Exception as e:
+        print(f"Error executing nvidia-smi: {e}")
+        raise
+
+
+def cudaq_reading_GPUS():
+    assert cudaq.num_available_gpus() > 0, "CUDAQ failed to detect any GPUs"
+
+
+def starpu_reading_GPUS():
+    try:
+        res = run("starpu_machine_display -w CUDA -topology")
+        number_of_GPUS = res.stdout.strip("\n")[1]
+        assert int(number_of_GPUS) > 0, "No GPUs detected by StarPU"
+    except Exception as e:
+        print(f"Error executing starpu_machine_display: {e}")
+        raise
+
+
+if __name__ == "__main__":
+    starpu.init()
+    asyncio.run(main())
+    validate_nvidia_smi()
+    cudaq_reading_GPUS()
+    print("Validation script executed successfully.")
+    starpu.shutdown()
